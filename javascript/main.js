bd8c8f7bb429aa173c06cdca51611c83627f16c3
unknown <nithinpathayathodi@gmail.com>
2024-12-11 00:32:57 +0530

    new updates

--- javascript/main.js
+++ javascript/main.js
@@ -19,3 +19,53 @@ if the email address contains @gmail.com then it should not be used */
 //     console.log("this can be use as this is not a gmail address");
 // }
 
+// let pass1 = "lolman@123";
+// let pass2 = "lolma@123";
+
+// if(pass1===pass2){
+//     console.log("password matched");
+// }else{
+//     console.log("pass1"+pass1);
+//     console.log("pass2"+pass2);
+//     console.log("password does not match");
+// }
+
+//array
+// let countires = ["india","london","canada","australia"]
+// console.log(countires);
+// countires.sort();
+// console.log(countires);
+
+//splitting string
+
+// let essay = "lorem ipsum dolor sit amet consectetur adipiscing elit";
+// let words = essay.split(" ");
+// let count = words.length;
+// console.log(count);
+
+//password validation
+// rules :
+// 1. password should have atleast 8 characters
+// 2. password should match the confirm password
+// let pass = "lol@1234";
+// let confirmPass = "lol@1234";
+
+// let passMatch = pass === confirmPass;
+// let passLength = pass.length >=8 ;
+
+// let validPass = passMatch && passLength;
+
+// if(validPass){
+//     console.log("password is valid");
+// } else {
+//     console.log("password is invalid");
+
+//  if(!passMatch){
+//     console.log("The passwords don't match");
+
+//  }
+
+//  if(!passLength){
+//     console.log("The password is not 8 characters long");
+//  }
+// }
